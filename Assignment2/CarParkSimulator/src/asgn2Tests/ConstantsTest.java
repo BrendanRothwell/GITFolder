2cadd95523b69092748af0bdb7397641b640c426
BrendanRothwell <mc.rotheee@gmail.com>
2014-05-13 15:46:13 +1000

    First Commit Redo

---
+++ Assignment2/CarParkSimulator/src/asgn2Tests/ConstantsTest.java
@@ -0,0 +1,26 @@
+package asgn2Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import asgn2Simulators.Constants;
+
+public class ConstantsTest {
+
+	private Constants co;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws SimulationException {
+		new co = Constants();
+	}
+	
+// Testing method accuracy
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+}
